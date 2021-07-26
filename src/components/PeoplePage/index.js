7f6bf293e55d236ce4001dc7479cf466bedf2014
Ruslan <rhgalyamov@gmail.com>
2021-07-26 22:55:35 +0300

    added: render function to ItemList

--- src/components/PeoplePage/index.js
+++ src/components/PeoplePage/index.js
@@ -36,6 +36,9 @@ export default class PeoplePage extends Component {
           <ItemList
             onItemSelected={this.onPersonSelected}
             getData={this.swapiService.getAllPeople}
+            renderItem={({ name, gender, birthYear }) =>
+              `${name} (${gender}, ${birthYear})`
+            }
           />
         </div>
         <div className='col-md-6'>
--- src/index.js
+++ src/index.js
@@ -44,6 +44,11 @@ export default class App extends Component {
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => (
+                <span>
+                  {item.name} <button>!</button>
+                </span>
+              )}
             />
           </div>
           <div className='col-md-6'>
@@ -56,6 +61,7 @@ export default class App extends Component {
             <ItemList
               onItemSelected={this.onPersonSelected}
               getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
             />
           </div>
           <div className='col-md-6'>
